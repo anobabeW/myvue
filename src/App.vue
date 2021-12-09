ab3316bd58559cc525bbfca59a9b18cb6c04771d
WangJiao <jiaowang1005@163.com>
2021-12-09 13:29:54 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>THIS IS T.O.P</h1>
+    <router-link to="/main">首页</router-link>
+    <router-link to="/content">内容页</router-link>
+    <!--展示template内容-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <h1>内容页</h1>
+    <h2>{{ title }}</h2>
+    <button @click="randomTitle()">改变Title</button>
+    <button @click="destoryVm()">摧毁实例</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  data() {
+    return {
+      title: "Hello World",
+    };
+  },
+  methods: {
+    randomTitle() {
+      this.title =
+        "Hello" +
+        ["China", "World", "Universe"][Math.floor(Math.random() * 2.999)];
+    },
+
+    destoryVm() {
+      this.$destroy();
+    },
+  },
+  beforeCreate() {
+    console.log("before create");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("before mount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("before update");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("before destory");
+  },
+  destroyed() {
+    console.log("destoryed");
+  },
+
+  /**
+   * 侦测变化的两种方式：
+   * Object.defineProperty
+   * ES6的Proxy
+   */
+  defineReactive(data, key, val) {
+    Object.defineProperty(data, key, {
+      enumerable:true,
+      configurable:true,
+      get:function() {
+        return val
+      },
+
+      set: function(newVal) {
+        if(val === newVal) {
+          return
+        }
+        val = newVal
+      }
+
+    })
+  },
+};
+</script>
+
+<!--作用域-->
+<style scoped>
+</style>
